a379341a1d04ce94d027ef14850ee06e5ef98d8a
Emmaly <emmaly@emma.ly>
2021-01-10 19:54:18 -0800

    Support passing querystring

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,12 @@ const db = admin.firestore();
 
 exports.redir = functions.https.onRequest((request, response) => {
     let slug = request.url.split("/", 2)[1].split("?", 2)[0].toLocaleLowerCase();
+    let query = "";
+    {
+        let q = request.url.split("?");
+        q.shift();
+        query = q.join("?");
+    }
     let hostname = request.hostname;
     return db.collection(hostname).doc(slug).get().then(documentSnapshot => {
         if (documentSnapshot.exists) {
@@ -14,7 +20,11 @@ exports.redir = functions.https.onRequest((request, response) => {
                 clickCount: admin.firestore.FieldValue.increment(1),
                 clickLast: admin.firestore.FieldValue.serverTimestamp()
             });
-            response.redirect(data.destination);
+            let destination = data.destination;
+            if (data.passQueryString && query !== "") {
+                destination = destination + (RegExp("\\?").test(destination) ? "&" : "?") + query;
+            }
+            response.redirect(destination);
         } else {
             if (slug === "404") {
                 response.redirect("/404.html"); // the 404 slug doesn't exist, so fail us to a static 404 page
